2c25c7bdf7a05a16552fc1d21371432275b44da2
Lmeerlu <l.meerlu@gmx.de>
2018-05-10 15:08:40 +0200

    Circles respond to touch + test on real device

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -5,15 +5,16 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
-public class MyGdxGame extends ApplicationAdapter implements GestureDetector.GestureListener{
+public class MyGdxGame extends ApplicationAdapter implements InputProcessor{
     final int FIELDX = 4;
-    final int FIELDY = 6;
+    final int FIELDY = 7;
 
     private int marginX;
     private int marginY;
@@ -21,18 +22,32 @@ public class MyGdxGame extends ApplicationAdapter implements GestureDetector.Ges
 	private SpriteBatch batch;
 	private ShapeRenderer shapeRenderer;
 	private Circle[][] field;
+	private BitmapFont count;
 
 	private int screenWidth, screenHeight;
+
+	private int countIndex, countX, countY;
+
 	
 	@Override
 	public void create () {
+
+
 		batch = new SpriteBatch();
+        Gdx.input.setInputProcessor(this);
 		shapeRenderer = new ShapeRenderer();
 		screenHeight = Gdx.graphics.getHeight();
         screenWidth = Gdx.graphics.getWidth();
 
-        marginY = 50;
+        marginY = 0;
         marginX = 0;
+
+        count = new BitmapFont();
+        countIndex = 0;
+        countX = 3;
+        countY = 325;
+        count.setColor(Color.BLACK);
+        count.getData().scale(5);
         int squareSize =(screenWidth - marginX)/FIELDX;
         int squareRadius = squareSize/2;
         field = new Circle[FIELDX][FIELDY];
@@ -53,15 +68,13 @@ public class MyGdxGame extends ApplicationAdapter implements GestureDetector.Ges
     public void dispose () {
         batch.dispose();
         shapeRenderer.dispose();
+        count.dispose();
     }
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-		batch.begin();
-		batch.end();
 
         //Gdx.gl.glLineWidth(32); //Line thickness
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
@@ -72,67 +85,92 @@ public class MyGdxGame extends ApplicationAdapter implements GestureDetector.Ges
 
                 if (field[j][i].getActivated()) {
                     shapeRenderer.setColor(Color.RED);
-                    shapeRenderer.circle(field[j][i].getX(), field[j][i].getY(), field[j][i].getRadius());
+                    shapeRenderer.circle(field[j][i].getX(), screenHeight - field[j][i].getY(), field[j][i].getRadius());
+
                 }else{
                     shapeRenderer.setColor(Color.BLUE);
-                    shapeRenderer.circle(field[j][i].getX(), field[j][i].getY(), field[j][i].getRadius());
+                    shapeRenderer.circle(field[j][i].getX(), screenHeight - field[j][i].getY(), field[j][i].getRadius());
                 }
             }
         }
+
+        batch.begin();
+        count.draw(batch,Integer.toString(countIndex),countX,countY);
+        batch.end();
+
         shapeRenderer.end();
 	}
 
-	//TODO wrong circle gets red, now try touchdown etc
-
+        //finger on this circle -> activate(red)
     public void activateCircle(int x, int y, Circle circle){
 
-        /*com.badlogic.gdx.math.Circle c = new com.badlogic.gdx.math.Circle(circle.getX(),circle.getY(),circle.getRadius());
-        if (c.contains(x,y))
-            circle.setActivated(true);*/
+        x = circle.getX() - x;
+        y = circle.getY() - y;
+
+        if (x * x + y * y <= circle.getRadius()* circle.getRadius()){
+            if (!circle.getActivated()) {
+                countIndex++;
+            }
+            circle.setActivated(true);
+        }
     }
 
     @Override
-    public boolean touchDown(float x, float y, int pointer, int button) {
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        for (int i = 0; i < FIELDY; i++) {
+            for (int j = 0; j < FIELDX; j++) {
+                activateCircle(Gdx.input.getX(), Gdx.input.getY(), field[j][i]);
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        for (int i = 0; i < FIELDY; i++) {
+            for (int j = 0; j < FIELDX; j++) {
+                field[j][i].setActivated(false);
+                countIndex = 0;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        for (int i = 0; i < FIELDY; i++) {
+            for (int j = 0; j < FIELDX; j++) {
+                activateCircle(Gdx.input.getX(), Gdx.input.getY(), field[j][i]);
+            }
+        }
+        return true;
+    }
+
+
+    @Override
+    public boolean keyDown(int keycode) {
         return false;
     }
 
     @Override
-    public boolean tap(float x, float y, int count, int button) {
+    public boolean keyUp(int keycode) {
         return false;
     }
 
     @Override
-    public boolean longPress(float x, float y) {
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
         return false;
     }
 
     @Override
-    public boolean fling(float velocityX, float velocityY, int button) {
+    public boolean scrolled(int amount) {
         return false;
-    }
-
-    @Override
-    public boolean pan(float x, float y, float deltaX, float deltaY) {
-        return false;
-    }
-
-    @Override
-    public boolean panStop(float x, float y, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean zoom(float initialDistance, float distance) {
-        return false;
-    }
-
-    @Override
-    public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
-        return false;
-    }
-
-    @Override
-    public void pinchStop() {
-
     }
 }
